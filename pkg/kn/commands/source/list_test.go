d9ec0dc597ce438cafcacfa45ff0dbf830b3e56c
Tonia Elengikal <elentonia@gmail.com>
2021-03-02 11:45:09 -0500

    changing list test to check for BUILT-IN SOURCE column

--- pkg/kn/commands/source/list_test.go
+++ pkg/kn/commands/source/list_test.go
@@ -64,9 +64,9 @@ func TestSourceListTypes(t *testing.T) {
 		newSourceCRDObjWithSpec("apiserversources", "sources.knative.dev", "v1alpha1", "ApiServerSource"),
 	)
 	assert.NilError(t, err)
-	assert.Check(t, util.ContainsAll(output[0], "TYPE", "NAME", "DESCRIPTION"))
-	assert.Check(t, util.ContainsAll(output[1], "ApiServerSource", "apiserversources"))
-	assert.Check(t, util.ContainsAll(output[2], "PingSource", "pingsources"))
+	assert.Check(t, util.ContainsAll(output[0], "TYPE", "NAME", "DESCRIPTION", "BUILT-IN SOURCE"))
+	assert.Check(t, util.ContainsAll(output[1], "ApiServerSource", "apiserversources", "YES"))
+	assert.Check(t, util.ContainsAll(output[2], "PingSource", "pingsources", "YES"))
 }
 
 func TestSourceListTypesNoHeaders(t *testing.T) {
@@ -74,7 +74,7 @@ func TestSourceListTypesNoHeaders(t *testing.T) {
 		newSourceCRDObjWithSpec("pingsources", "sources.knative.dev", "v1alpha1", "PingSource"),
 	)
 	assert.NilError(t, err)
-	assert.Check(t, util.ContainsNone(output[0], "TYPE", "NAME", "DESCRIPTION"))
+	assert.Check(t, util.ContainsNone(output[0], "TYPE", "NAME", "DESCRIPTION", "BUILT-IN SOURCE"))
 	assert.Check(t, util.ContainsAll(output[0], "PingSource"))
 }
 
